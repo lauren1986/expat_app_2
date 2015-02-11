e798ed3b02dd697d17cc3c2901a05bf61168ba7e
laurenpawell <lauren.pawell@gmail.com>
2015-02-11 12:37:52 -0800

    Started adding login process

--- expat/app/controllers/sessions_controller.rb
+++ expat/app/controllers/sessions_controller.rb
@@ -4,12 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    raise params.inspect
     # Find the user by email
     u = User.where(email: params[:user][:email]).first
     if u != nil && u.authenticate(params[:user][:password])
+        session["user_id"] = u.id.to_s
+        redirect_to new_users_path
+    end
   end
 
   def destroy
+    session.destroy
   end
 end
