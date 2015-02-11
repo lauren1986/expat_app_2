4204f7408c091bd1b42a1441ffef29fc307d998c
laurenpawell <lauren.pawell@gmail.com>
2015-02-11 08:50:02 -0800

    Created Rails app

---
+++ expat/app/controllers/expats_controller.rb
@@ -0,0 +1,19 @@
+class ExpatsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def create
+  end
+end
---
+++ expat/app/helpers/expats_helper.rb
@@ -0,0 +1,2 @@
+module ExpatsHelper
+end
